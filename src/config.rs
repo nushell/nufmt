dca0413905fd9533279dff9d01bb433bf00e4227
Auca Maillot <aucacoyan@gmail.com>
2023-05-25 12:36:08 -0300

    :sparkles: standarized Config with minimal defaults

--- src/config.rs
+++ src/config.rs
@@ -1,21 +1,38 @@
-/// options available to the formatter
+use anyhow::Result;
+
+/// Configurations available to the formatter
 pub struct Config {
-    /// number of spaces of indent
+    /// Number of spaces of indent.
     pub tab_spaces: usize,
-    /// max amount of characters per line
-    /// # Maximum width of each line.
-    /// Default: 100
+    /// Maximum width of each line.
     pub max_width: usize,
-    /// number of lines bafore and after a custom command
+    /// Number of lines bafore and after a custom command.
     pub margin: usize,
 }
 
 impl Default for Config {
     fn default() -> Self {
         Config {
-            tab_spaces: 2,
-            max_width: 100,
+            tab_spaces: 4,
+            max_width: 80,
             margin: 1,
         }
     }
+}
+
+impl Config {
+    /// Creates a new config. You need to pass every field to create the config.
+    /// You cannot skip any field yet.
+    pub fn new(tab_spaces: usize, max_width: usize, margin: usize) -> Self {
+        Config {
+            tab_spaces: tab_spaces,
+            max_width: max_width,
+            margin: margin,
+        }
+    }
+}
+
+/// Returns a default config.
+pub fn load_config(/* file_path: Option<&Path> */) -> Result<Config> {
+    Ok(Config::default())
 }
