1f446e71ade6468c219d6472ec8a3a0e5acc6b60
Loïc Riegel <loic.riegel@outlook.fr>
2025-05-03 14:32:20 +0200

    cargo update

---
+++ src/config_error.rs
@@ -0,0 +1,27 @@
+use thiserror::Error;
+
+#[derive(Clone, Debug, Error, PartialEq, Eq)]
+pub enum ConfigError {
+    #[error("Failed read the configuration file: {0}")]
+    IOError(String),
+    #[error("The configuration is not valid nuon record")]
+    InvalidFormat,
+    #[error("Unknown configuration option: {0}")]
+    UnknownOption(String),
+    #[error("Invalid type for '{0}': got {1}, expected {2}")]
+    InvalidOptionType(String, String, &'static str),
+    #[error("Invalid value for '{0}': got {1}, expected {2}")]
+    InvalidOptionValue(String, String, &'static str),
+}
+
+impl From<std::io::Error> for ConfigError {
+    fn from(value: std::io::Error) -> Self {
+        ConfigError::IOError(value.to_string())
+    }
+}
+
+impl From<nu_protocol::ShellError> for ConfigError {
+    fn from(_value: nu_protocol::ShellError) -> Self {
+        ConfigError::InvalidFormat
+    }
+}
