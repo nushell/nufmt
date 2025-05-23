62fd38af2f6536bb19ecc78a4dd0f0e1245c0939
x10an14 <x10an14@users.noreply.github.com>
2025-05-23 17:53:28 +0200

    Fix Nix build & direnv config (#70)
    - **flake: Update inputs**
    - **fix(direnv): Specify shell w/shebang + use direnv stdlib + use
    correct syntax**
    - **flake: Simplify input dependencies**
    - **refactor(flake): Add formatter for nix + tidy up flake**
    - **fix(flake): Nix builds release + always specifies target triple**
    
    ## Description of changes
    - Updated inputs used in nix flake (tool that defines the nix package of
    nufmt)
    - Added formatter for nix flake/nix code
    - Simplified nix flake's inputs/dependencies (synchronized and
    deduplicated)
    - Tidied up/fixed .envrc file/direnv
    - Fixed build broken by new tests added in #69
    
    ## Relevant Issues
    None

--- flake.nix
+++ flake.nix
@@ -5,37 +5,72 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
 
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    { self, ... }@inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
       let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
+        overlays = [ (import inputs.rust-overlay) ];
+        pkgs = import inputs.nixpkgs {
           inherit system overlays;
+        };
+        integrationTestPatch = pkgs.writeTextFile {
+          name = "integration-tests-nix-fix.patch";
+          text =
+            # patch
+            ''
+              diff --git a/tests/main.rs b/tests/main.rs
+              index c3a2b64..173aea0 100644
+              --- a/tests/main.rs
+              +++ b/tests/main.rs
+              @@ -8,7 +8,7 @@ let one = 1
+               const VALID: &str = "# beginning of script comment
+               let one = 1
+               ";
+              -const TEST_BINARY: &'static str = "target/debug/nufmt";
+              +const TEST_BINARY: &'static str = "target/@target_triple@/release/nufmt";
+
+               #[test]
+               fn failure_with_invalid_config() {
+            '';
         };
       in
       {
         devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            rust-bin.stable.latest.default
-            rust-analyzer
-
+          inputsFrom = [ self.packages.${system}.default ];
+          packages = with pkgs; [
             nushell
-          ];
 
-          buildInputs = with pkgs; [ ];
+            # Not included in the package dependencies, but used for development
+            rust-analyzer
+          ];
         };
 
-        packages.default = pkgs.rustPlatform.buildRustPackage rec {
+        packages.default = self.packages.${system}.nufmt;
+        packages.nufmt = pkgs.rustPlatform.buildRustPackage {
           name = "nufmt";
-
           src = ./.;
+          patches = [
+            (pkgs.replaceVars "${integrationTestPatch}" {
+              target_triple = pkgs.stdenv.hostPlatform.rust.rustcTarget;
+            })
+          ];
+          cargoLock.lockFile = ./Cargo.lock;
+        };
 
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-          };
+        formatter = inputs.treefmt-nix.lib.mkWrapper pkgs {
+          programs.nixfmt.enable = true;
         };
       }
     );
