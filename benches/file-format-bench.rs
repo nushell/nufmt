b06da99e1bdbfab6199a919c082aef748b821249
Sha1rholder <sha1rholder@outlook.com>
2026-09-03 12:41:04 +0800

    - merge pr ci and push ci - update main ci dependencies - fix: file-format-bench.rs edits example.nu while it shouldn't - add rustfmt.toml to override global hard_tabs rule

--- benches/file-format-bench.rs
+++ benches/file-format-bench.rs
@@ -1,15 +1,13 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use nu_formatter::{config::Config, format_single_file, Mode};
-use std::path::PathBuf;
+use nu_formatter::{config::Config, format_string};
 
 fn criterion_benchmark(c: &mut Criterion) {
+    let input = include_str!("example.nu");
+    let config = Config::default();
+
     c.bench_function("Format massive nu", |b| {
         b.iter(|| {
-            format_single_file(
-                PathBuf::from("./benches/example.nu"),
-                &Config::default(),
-                &Mode::Normal,
-            )
+            format_string(input, &config).expect("benchmark fixture should contain valid Nushell")
         });
     });
 }
