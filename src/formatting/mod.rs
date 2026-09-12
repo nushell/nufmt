698e46150cdd0fbebbb090a81212b8e66b9276e2
Andrii Dokhniak <adokhniak@gmail.com>
2026-09-13 01:52:09 +0200

    Only try to fixup broken AST issue 220

--- src/formatting/mod.rs
+++ src/formatting/mod.rs
@@ -277,14 +277,16 @@ fn format_inner_with_options(contents: &[u8], config: &Config) -> Result<Vec<u8>
         .iter()
         .map(ParseError::span)
         .collect();
-    malformed_spans.extend(detect_compact_if_else_spans(&source_text));
-    malformed_spans.extend(detect_missing_record_comma_spans(&source_text));
-    malformed_spans.extend(detect_redundant_pipeline_subexpr_spans(&source_text));
 
     let has_garbage = block_contains_garbage(&working_set, &parsed_block);
     let has_fatal_parse_error = working_set.parse_errors.iter().any(is_fatal_parse_error);
 
     if !malformed_spans.is_empty() || has_garbage {
+        // Only try to detect fixable errors if the AST is broken
+        malformed_spans.extend(detect_compact_if_else_spans(&source_text));
+        malformed_spans.extend(detect_missing_record_comma_spans(&source_text));
+        malformed_spans.extend(detect_redundant_pipeline_subexpr_spans(&source_text));
+
         if let Some(repaired) = try_repair_parse_errors(contents, &malformed_spans) {
             debug!(
                 "retrying formatting after targeted parse-error repair ({} parse errors)",
